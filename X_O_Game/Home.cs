eb4ddeb64e5aeaa4a3a7c89be639b31a00c068dd
a7medbika <ahmedbkar11@gmail.com>
2021-08-02 04:28:26 +0200

    Removed Bunifu

--- X_O_Game/Home.cs
+++ X_O_Game/Home.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,20 @@ namespace X_O_Game
 {
     public partial class Home : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
         public Home()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
 
         private void Home_Load(object sender, EventArgs e)
---
+++ X_O_Game/RoundedButton.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O_Game
+{
+    class RoundedButton : Button
+    {
+        private int bSize = 0;
+        private int bRedius = 50;
+        private Color bColor = Color.DarkRed;
+
+        [Category("Bika Setting")]
+        public int BSize { get => bSize; set { bSize = value; this.Invalidate(); }  }
+        [Category("Bika Setting")]
+        public int BRedius { get => bRedius; set { bRedius = value; this.Invalidate(); } }
+        [Category("Bika Setting")]
+        public Color BColor { get => bColor; set { bColor = value; this.Invalidate(); } }
+        [Category("Bika Setting")]
+        public Color BackgroundColor
+        {
+            get { return this.BackColor; }
+            set { this.BackColor = value; }
+        }
+        [Category("Bika Setting")]
+        public Color TextColor
+        {
+            get { return this.ForeColor; }
+            set { this.ForeColor = value; }
+        }
+
+        public RoundedButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+        }
+
+        private GraphicsPath GetFigurePath(RectangleF rec, float reduis)
+        {
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rec.X, rec.Y, reduis, reduis, 180, 90);
+            path.AddArc(rec.Width - reduis, rec.Y, reduis, reduis, 270, 90);
+            path.AddArc(rec.Width - reduis, rec.Height - reduis, reduis, reduis, 0, 90);
+            path.AddArc(rec.X, rec.Height - reduis, reduis, reduis, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF recsource = new RectangleF(0, 0, this.Width, this.Height);
+            RectangleF recborder = new RectangleF(0, 0, this.Width - 0.8f, this.Height - 1);
+
+            if(bRedius > 2) // rounded
+            {
+                using (GraphicsPath pathsource = GetFigurePath(recsource, bRedius))
+                using (GraphicsPath pathborder = GetFigurePath(recborder,bRedius-1f))
+                using (Pen pensource = new Pen(this.Parent.BackColor,2))
+                using (Pen penborder = new Pen(bColor, bSize))
+                {
+                    penborder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathsource);
+
+                    // Draw Serfus
+                    pevent.Graphics.DrawPath(pensource, pathsource);
+
+                    // Draw Border
+                    if (bSize >= 1) pevent.Graphics.DrawPath(penborder, pathborder);
+                }
+            }
+
+            else // normal
+            {
+                this.Region = new Region(recsource);
+
+                if(bSize >= 1)
+                {
+                    using (Pen penborder = new Pen(bColor, bSize))
+                    {
+                        penborder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penborder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+            }
+        }
+    }
+}
